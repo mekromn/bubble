cdb74ac5d959751bf4465e8e6455e94b4a209144
mekromn <67240529+mekromn@users.noreply.github.com>
2026-09-07 04:35:04 -0700

    test: lock four-direction floating page pill gestures

--- app/src/test/java/com/mekromn/bubble/ToolbarSwipePolicyTest.kt
+++ app/src/test/java/com/mekromn/bubble/ToolbarSwipePolicyTest.kt
@@ -11,10 +11,16 @@ class ToolbarSwipePolicyTest {
         assertEquals(ToolbarSwipe.NONE, ToolbarSwipePolicy.classify(80f, 76f, 32f, horizontalTabs = true))
     }
 
-    @Test fun floatingPillSeparatesUpSwitcherReturnAndDownMinimize() {
-        assertEquals(ToolbarSwipe.OPEN_CHOOSER, ToolbarSwipePolicy.classify(5f, -70f, 24f, swipeUpChooser = true, swipeDownMinimize = true))
+    @Test fun floatingPagePillMapsAllFourDirections() {
+        assertEquals(ToolbarSwipe.PAGE_BACK, ToolbarSwipePolicy.classify(-72f, 5f, 24f, horizontalHistory = true, swipeUpChooser = true, swipeDownMinimize = true))
+        assertEquals(ToolbarSwipe.PAGE_FORWARD, ToolbarSwipePolicy.classify(72f, -5f, 24f, horizontalHistory = true, swipeUpChooser = true, swipeDownMinimize = true))
+        assertEquals(ToolbarSwipe.OPEN_CHOOSER, ToolbarSwipePolicy.classify(5f, -70f, 24f, horizontalHistory = true, swipeUpChooser = true, swipeDownMinimize = true))
+        assertEquals(ToolbarSwipe.MINIMIZE, ToolbarSwipePolicy.classify(-6f, 70f, 24f, horizontalHistory = true, swipeUpChooser = true, swipeDownMinimize = true))
+    }
+
+    @Test fun chooserPillReturnsUpAndMinimizesDown() {
         assertEquals(ToolbarSwipe.RETURN_TO_TAB, ToolbarSwipePolicy.classify(-8f, -64f, 24f, swipeUpReturn = true, swipeDownMinimize = true))
-        assertEquals(ToolbarSwipe.MINIMIZE, ToolbarSwipePolicy.classify(-6f, 70f, 24f, swipeUpChooser = true, swipeDownMinimize = true))
-        assertEquals(ToolbarSwipe.NONE, ToolbarSwipePolicy.classify(80f, -40f, 24f, swipeUpChooser = true, swipeDownMinimize = true))
+        assertEquals(ToolbarSwipe.MINIMIZE, ToolbarSwipePolicy.classify(6f, 64f, 24f, swipeUpReturn = true, swipeDownMinimize = true))
+        assertEquals(ToolbarSwipe.NONE, ToolbarSwipePolicy.classify(80f, -40f, 24f, swipeUpReturn = true, swipeDownMinimize = true))
     }
 }
