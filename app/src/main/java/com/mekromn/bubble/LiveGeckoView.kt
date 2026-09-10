2b1a82433541928ca71baf155d0399dd36159121
mekromn <67240529+mekromn@users.noreply.github.com>
2026-09-10 14:14:18 -0700

    diag: trace fullscreen GeckoView session ownership

--- app/src/main/java/com/mekromn/bubble/LiveGeckoView.kt
+++ app/src/main/java/com/mekromn/bubble/LiveGeckoView.kt
@@ -3,6 +3,7 @@ package com.mekromn.bubble
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
+import org.mozilla.geckoview.GeckoSession
 import org.mozilla.geckoview.GeckoView
 
 /**
@@ -20,18 +21,58 @@ internal open class LiveGeckoView(context: Context) : GeckoView(context) {
         main.post(reconcile)
     }
 
+    override fun setSession(session: GeckoSession) {
+        DiagnosticLog.event(
+            "GECKOVIEW",
+            "setSession begin view=${Integer.toHexString(System.identityHashCode(this))} attached=$isAttachedToWindow " +
+                "old=${DiagnosticLog.sessionLabel(this.session)} new=${DiagnosticLog.sessionLabel(session)}"
+        )
+        super.setSession(session)
+        DiagnosticLog.event(
+            "GECKOVIEW",
+            "setSession complete view=${Integer.toHexString(System.identityHashCode(this))} attached=$isAttachedToWindow current=${DiagnosticLog.sessionLabel(this.session)}"
+        )
+    }
+
+    override fun releaseSession(): GeckoSession? {
+        val before = session
+        DiagnosticLog.event(
+            "GECKOVIEW",
+            "releaseSession begin view=${Integer.toHexString(System.identityHashCode(this))} attached=$isAttachedToWindow current=${DiagnosticLog.sessionLabel(before)}"
+        )
+        val released = super.releaseSession()
+        DiagnosticLog.event(
+            "GECKOVIEW",
+            "releaseSession complete view=${Integer.toHexString(System.identityHashCode(this))} released=${DiagnosticLog.sessionLabel(released)}"
+        )
+        return released
+    }
+
     override fun onWindowVisibilityChanged(visibility: Int) {
+        DiagnosticLog.event(
+            "GECKOVIEW",
+            "windowVisibility=$visibility view=${Integer.toHexString(System.identityHashCode(this))} attached=$isAttachedToWindow ${DiagnosticLog.sessionLabel(session)}"
+        )
         super.onWindowVisibilityChanged(visibility)
         reconcileLater()
     }
 
     override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
+        DiagnosticLog.event(
+            "GECKOVIEW",
+            "windowFocus=$hasWindowFocus view=${Integer.toHexString(System.identityHashCode(this))} attached=$isAttachedToWindow ${DiagnosticLog.sessionLabel(session)}"
+        )
         super.onWindowFocusChanged(hasWindowFocus)
         reconcileLater()
     }
 
     override fun onDetachedFromWindow() {
+        DiagnosticLog.event(
+            "GECKOVIEW",
+            "onDetachedFromWindow begin view=${Integer.toHexString(System.identityHashCode(this))} ${DiagnosticLog.sessionLabel(session)}"
+        )
         super.onDetachedFromWindow()
+        DiagnosticLog.event("GECKOVIEW", "onDetachedFromWindow complete view=${Integer.toHexString(System.identityHashCode(this))}")
         reconcileLater()
     }
 }
